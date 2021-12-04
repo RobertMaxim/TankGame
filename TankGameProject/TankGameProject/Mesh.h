e2916937ae41d53e3f17b0a5ec215873d07b9672
Maxim Robert <robert.maxim@student.unitbv.ro>
2021-12-04 12:41:32 +0200

    Added Camera class, texture class, mesh class and headers

--- TankGameProject/TankGameProject/Mesh.h
+++ TankGameProject/TankGameProject/Mesh.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "Vertex.h"
+#include "Shader.h"
+#include "Camera.h"
+#include <vector>
+
+class Mesh {
+public:
+	std::vector <Vertex> vertices;
+	std::vector <GLuint> indices;
+	std::vector <Texture> textures;
+	// Store VAO in public so it can be used in the Draw function
+	GLuint vaoID;
+
+	// Initializes the mesh
+	Mesh(std::vector <Vertex>& vertices, std::vector <GLuint>& indices, std::vector <Texture>& textures);
+
+	// Draws the mesh
+	void Draw(Shader& shader, Camera& camera);
+
+};
--- TankGameProject/TankGameProject/Texture.h
+++ TankGameProject/TankGameProject/Texture.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "Headers.h"
+#include "Shader.h"
+
+class Texture
+{
+public:
+	GLuint ID;
+	GLenum type;
+	Texture(const char* image, GLenum texType, GLenum slot, GLenum format, GLenum pixelType);
+
+	// Assigns a texture unit to a texture
+	void texUnit(GLuint shaderID, const char* uniform, GLuint unit);
+	// Binds a texture
+	void Bind();
+	// Unbinds a texture
+	void Unbind();
+	// Deletes a texture
+	void Delete();
+};
