ef5d7f06b8f84f114c70b5f38ddc332671253598
Maxim Robert <robert.maxim@student.unitbv.ro>
2021-12-31 02:02:51 +0200

    Added skybox

--- TankGameProject/TankGameProject/TankGameProject.cpp
+++ TankGameProject/TankGameProject/TankGameProject.cpp
@@ -299,9 +299,6 @@
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-
-
-
 Camera* pCamera = nullptr;
 
 void Cleanup()
@@ -319,6 +316,41 @@ double deltaTime = 0.0f;    // time between current frame and last frame
 double lastFrame = 0.0f;
 
 unsigned int CreateTexture(const std::string& strTexturePath);
+
+float skyboxVertices[] =
+{
+    //   Coordinates
+    -100.0f, -100.0f,  100.0f,//        7--------6
+     100.0f, -100.0f,  100.0f,//       /|       /|
+     100.0f, -100.0f, -100.0f,//      4--------5 |
+    -100.0f, -100.0f, -100.0f,//      | |      | |
+    -100.0f,  100.0f,  100.0f,//      | 3------|-2
+     100.0f,  100.0f,  100.0f,//      |/       |/
+     100.0f,  100.0f, -100.0f,//      0--------1
+    -100.0f,  100.0f, -100.0f
+};
+
+unsigned int skyboxIndices[] =
+{
+    // Right
+    1, 2, 6,
+    6, 5, 1,
+    // Left
+    0, 4, 7,
+    7, 3, 0,
+    // Top
+    4, 5, 6,
+    6, 7, 4,
+    // Bottom
+    0, 3, 2,
+    2, 1, 0,
+    // Back
+    0, 1, 5,
+    5, 4, 0,
+    // Front
+    3, 7, 6,
+    6, 2, 3
+};
 
 int main(int argc, char** argv)
 {
@@ -430,11 +462,7 @@ int main(int argc, char** argv)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-
-
-
-
-
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 
     //Mountain texture
     unsigned int mountainTexture = CreateTexture(strExePath + "\\mountain.jpg");
@@ -449,7 +477,90 @@ int main(int argc, char** argv)
 
     Shader shaderFloor("Floor.vs", "Floor.fs");
     Shader shaderBlending("blending.vs", "blending.fs");
-    shaderBlending.SetInt("texture1", 0);
+    Shader skyboxShader("skybox.vs", "skybox.fs");
+
+    //shaderBlending.SetInt("texture1", 0);
+    glUseProgram(shaderBlending.GetID());
+    glUniform1i(glGetUniformLocation(shaderBlending.GetID(), "texture1"), 0);
+
+    glUseProgram(skyboxShader.GetID());
+    glUniform1i(glGetUniformLocation(skyboxShader.GetID(), "skybox"), 0);
+
+    glEnable(GL_DEPTH_CLAMP);
+    glEnable(GL_CULL_FACE);
+    // Keeps front faces
+    glCullFace(GL_FRONT);
+    // Uses counter clock-wise standard
+    glFrontFace(GL_CCW);
+
+
+    unsigned int skyboxVAO, skyboxVBO, skyboxEBO;
+    glGenVertexArrays(1, &skyboxVAO);
+    glGenBuffers(1, &skyboxVBO);
+    glGenBuffers(1, &skyboxEBO);
+    glBindVertexArray(skyboxVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, skyboxEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(skyboxIndices), &skyboxIndices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    std::string facesCubemap[6] =
+    {
+        strExePath + "\\right.jpg",
+        strExePath + "\\left.jpg",
+        strExePath + "\\top.jpg",
+        strExePath + "\\bottom.jpg",
+        strExePath + "\\front.jpg",
+        strExePath + "\\back.jpg"
+    };
+
+    // Creates the cubemap texture object
+    unsigned int cubemapTexture;
+    glGenTextures(1, &cubemapTexture);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    // These are very important to prevent seams
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+    // This might help with seams on some systems
+    //glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
+
+    // Cycles through all the textures and attaches them to the cubemap object
+    for (unsigned int i = 0; i < 6; i++)
+    {
+        int width, height, nrChannels;
+        unsigned char* data = stbi_load(facesCubemap[i].c_str(), &width, &height, &nrChannels, 0);
+        if (data)
+        {
+            stbi_set_flip_vertically_on_load(false);
+            glTexImage2D
+            (
+                GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+                0,
+                GL_RGB,
+                width,
+                height,
+                0,
+                GL_RGB,
+                GL_UNSIGNED_BYTE,
+                data
+            );
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Failed to load texture: " << facesCubemap[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+
 
     // render loop
     while (!glfwWindowShouldClose(window)) {
@@ -460,10 +571,9 @@ int main(int argc, char** argv)
 
         // input
         processInput(window);
-        glClearColor(0.1f, 0.1f, 1.0f, 1.0f);
-
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        // Clean the back buffer and depth buffer
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
         glm::mat4 model = glm::mat4(1.0);
 
         shaderFloor.Use();
@@ -492,9 +602,6 @@ int main(int argc, char** argv)
         model = glm::mat4();
         shaderFloor.SetMat4("model", model);
         glDrawArrays(GL_TRIANGLES, 0, 6);
-
-
-
 
 
         shaderBlending.Use();
@@ -530,6 +637,29 @@ int main(int argc, char** argv)
             glDrawArrays(GL_TRIANGLES, 0, 6);
         }
 
+        glDepthFunc(GL_LEQUAL);
+
+        glUseProgram(skyboxShader.GetID());
+
+        view = glm::mat4(1.0f);
+        projection = glm::mat4(1.0f);
+        // We make the mat4 into a mat3 and then a mat4 again in order to get rid of the last row and column
+        // The last row and column affect the translation of the skybox (which we don't want to affect)
+        view = pCamera->GetViewMatrix();
+        projection = pCamera->GetProjectionMatrix();
+        glUniformMatrix4fv(glGetUniformLocation(skyboxShader.GetID(), "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(skyboxShader.GetID(), "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+        // Draws the cubemap as the last object so we can save a bit of performance by discarding all fragments
+        // where an object is present (a depth of 1.0f will always fail against any object's depth value)
+        glBindVertexArray(skyboxVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        glDepthFunc(GL_LESS);
+
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -539,7 +669,7 @@ int main(int argc, char** argv)
     Cleanup();
 
     // Clear floor VAO
-
+    
     // Clear grass VAO
 
     // glfw: terminate, clearing all previously allocated GLFW resources
